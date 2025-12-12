8680125a14bf95c247ee51d222cdcd1890a0d3eb
sdeslvm <dm1tstep@yandex.ru>
2025-12-12 03:37:44 +0300

    Initial Commit

---
+++ ZMTemplate/ZMTemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZMTemplateApp.swift
+//  ZMTemplate
+//
+//  Created by alex on 12/12/25.
+//
+
+import SwiftUI
+
+@main
+struct ZMTemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
